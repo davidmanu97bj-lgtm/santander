bf7de674c30b829cd80f786b90fc81f499b0ed73
davidmanu97bj-lgtm <davidmanu97bj@gmail.com>
2026-07-30 23:18:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Compatibilidad validada: v4088-apertura-instantanea */
 /* EXPLORA PWA service worker · v2.5.6 */
 const CACHE_PREFIX = 'explora-pwa-';
-const CACHE_NAME = `${CACHE_PREFIX}v4093-uber-caja-chica`;
+const CACHE_NAME = `${CACHE_PREFIX}v4094-uber-resumen-claro`;
 
 const LEGACY_MILEAGE_STUB = `
 const noop=()=>{};const asyncTrue=async()=>true;
