79a68518e29ca8ca5cfd6c7684c68172afd7718a
davidmanu97bj-lgtm <davidmanu97bj@gmail.com>
2026-08-13 08:07:45 -0300

    Add files via upload

--- js/pwa-register.js
+++ js/pwa-register.js
@@ -3,7 +3,7 @@
   'use strict';
   if (!('serviceWorker' in navigator)) return;
 
-  const BUILD = 'v4132-android-user-gesture-intent';
+  const BUILD = 'v4133-android-auto-whatsapp-https';
 
   const clearLegacyCaches = async () => {
     try {
